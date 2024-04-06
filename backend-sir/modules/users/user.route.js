4c0946e0d644b12c74822ac917ca1bc83a1ba8e9
Subash Karki <subashkarki68@gmail.com>
2024-04-07 01:58:19 +0545

    "Removed cookies' signed, httpOnly, sameSite, and secure attributes in user login route"

--- backend-sir/modules/users/user.route.js
+++ backend-sir/modules/users/user.route.js
@@ -74,10 +74,6 @@ router.post("/login", login, async (req, res, next) => {
   try {
     const result = await userController.login(req.body);
     res.cookie("access_token", generateAccessToken(result.user), {
-      signed: true,
-      httpOnly: true,
-      sameSite: "strict",
-      secure: true,
       expires: expiryTime,
     });
     res.json({ result });
